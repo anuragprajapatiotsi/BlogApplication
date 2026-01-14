2fe35d8f3465095922a2116f523b21680fd0c303
Anurag Prajapati <anurag.prajapati@otsi.com>
2026-01-14 12:27:10 +0530

    Update the import statment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button } from "./button";
+import { Button } from "./ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
